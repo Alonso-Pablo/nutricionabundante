bdbf90240a99c4e1851dfe6c86f9c1441f171f30
Alonso-Pablo <sidokunoice@gmail.com>
2021-03-08 16:44:29 -0300

    2
    añadiendo js y mejorando la pagina

--- js/nutricion.js
+++ js/nutricion.js
@@ -6,3 +6,11 @@ console.log(screen.width)
 const ul_nav = document.getElementById('ul_nav')
 if (window.innerWidth < 376)
 ul_nav.removeChild(ul_nav.children[0]);
+
+const backgroundFirstPanel = document.getElementById('backgroundFirstPanel');
+const firstPanelLeft = document.getElementById('firstPanelLeft');
+
+firstPanelLeft.addEventListener('click', ()=>{
+    backgroundFirstPanel.classList.replace('body__firts_panel','body__firts_panel-2');
+    console.log('Si');
+})
