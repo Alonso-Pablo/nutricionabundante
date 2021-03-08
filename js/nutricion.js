07eec59776f655d5a807b2aac7349fb50b608538
Alonso-Pablo <sidokunoice@gmail.com>
2021-03-08 13:20:08 -0300

    actualizacion_1
    1

--- js/nutricion.js
+++ js/nutricion.js
@@ -1,4 +1,8 @@
 const body_disclaimer = document.getElementById('body_disclaimer');
-body_disclaimer .addEventListener('click', ()=>{
+body_disclaimer.addEventListener('click', ()=>{
     body_disclaimer.remove()
 })
+console.log(screen.width)
+const ul_nav = document.getElementById('ul_nav')
+if (window.innerWidth < 376)
+ul_nav.removeChild(ul_nav.children[0]);
